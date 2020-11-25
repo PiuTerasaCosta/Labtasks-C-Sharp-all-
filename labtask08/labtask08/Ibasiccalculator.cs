8314a3a6ffb7b8d6f3f6291e58b427abf8b98e52
Piu Terasa Costa <piucosta98@gail.com>
2020-11-25 11:59:09 +0600

    labtask08

---
+++ labtask08/labtask08/Ibasiccalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labtask08
+{
+    interface Ibasiccalculator
+    {
+        int sum(int x, int y);
+        int sub(int x, int y);
+        int multiplication(int x, int y);
+        int division(int x, int y);
+
+    }
+}
---
+++ labtask08/labtask08/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labtask08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
