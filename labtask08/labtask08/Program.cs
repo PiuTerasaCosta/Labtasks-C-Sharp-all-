5c4e03d2f32631461d03b683f46be0a0463dda1e
Piu Terasa Costa <piucosta98@gail.com>
2020-11-30 00:18:38 +0600

    final labtak08

--- labtask08/labtask08/Program.cs
+++ labtask08/labtask08/Program.cs
@@ -10,6 +10,15 @@ namespace labtask08
     {
         static void Main(string[] args)
         {
+            calculator calcu = new calculator();
+            Console.WriteLine(" Sum = " + calcu.sum(10, 5));
+            Console.WriteLine(" Sub = " + calcu.sub(10, 5));
+            Console.WriteLine(" Division = " + calcu.division(25,5));
+            Console.WriteLine(" Multiplication = " + calcu.multiplication(10, 5));
+            Console.WriteLine(" Division = " + calcu.division(10, 5));
+            Console.WriteLine(" Power = " + calcu.XtoY(10, 5));
+            Console.WriteLine(" Factorial = " + calcu.factorial(5));
+
 
         }
     }
---
+++ labtask08/labtask08/calculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labtask08
+{
+    class calculator : Ibasiccalculator : Iscientificcalculator
+   {
+        int result;
+
+    public int division(int x, int y)
+    {
+        return x / y;
+    }
+
+    public int factorial(int x)
+    {
+        int i = 1;
+        for (int s = 1; s <= x; s++)
+        {
+            i = i * s;
+        }
+        return i;
+    }
+
+    public int multiplication(int x, int y)
+    {
+        return x * y;
+    }
+
+    public int sub(int x, int y)
+    {
+        return x - y;
+    }
+
+    public int sum(int x, int y)
+    {
+        return x + y;
+    }
+    public int division(int x, int y)
+    {
+        return x / y;
+    }
+
+    public int XtoY(int x, int y)
+    {
+        while (y != 0)
+        {
+            result *= x;
+            --y;
+        }
+        return result;
+    }
+} }
+
+
+
+
